f1f0eca9ba3dfb05e92f9fb82dc09f9d9f2616e5
squate <tobogganlad@gmail.com>
2019-02-10 17:20:16 -0500

    added test comment

--- app/src/main/java/com/example/trails/MainActivity.java
+++ app/src/main/java/com/example/trails/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.trails;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//github commit testing, delete after
 public class MainActivity extends AppCompatActivity {
 
     @Override
