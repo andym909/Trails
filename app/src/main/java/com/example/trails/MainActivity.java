f04093653c6b98c88abb3ec1c0b44480b918c273
Andy Martin <andymartin909@gmail.com>
2019-02-10 19:14:02 -0500

    button opens the new activity

--- app/src/main/java/com/example/trails/MainActivity.java
+++ app/src/main/java/com/example/trails/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.trails;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 //github commit testing, delete after
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openARview(View view) {
+        Intent intent = new Intent(this, ARViewActivity.class);
+        startActivity(intent);
     }
 }
