1c20c4c44336e01b20d4e820910a4402b19ad524
Andy Martin <andymartin909@gmail.com>
2019-02-10 21:15:53 -0500

    heres the fucked up code

--- app/src/main/java/com/example/trails/MainActivity.java
+++ app/src/main/java/com/example/trails/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openARview(View view) {
-        Intent intent = new Intent(this, ARViewActivity.class);
+        Intent intent = new Intent(this, SimpleAR.class);
         startActivity(intent);
     }
 }
