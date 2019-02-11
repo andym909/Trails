1251543cea23926cfe19c2e4eb5807746bacb7d9
eeinhorn <ee237534@muhlenberg.edu>
2019-02-10 19:10:37 -0500

    trying to make the button work

--- app/src/main/java/com/example/trails/MainActivity.java
+++ app/src/main/java/com/example/trails/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.trails;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 //github commit testing, delete after
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void openARView(View view) {
+        Intent intent = new Intent(this, ARViewActivity.class);
+        startActivity(intent);
     }
 }
