7b0285acf6e65a41fd27d70f032a779dc76d1e19
squate <tobogganlad@gmail.com>
2019-02-17 17:54:31 -0500

    nuclear option

--- app/src/main/java/com/example/trails/ShowCoordActivity.java
+++ app/src/main/java/com/example/trails/ShowCoordActivity.java
@@ -6,13 +6,9 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationListener;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import com.wikitude.tools.location.LocationService;
